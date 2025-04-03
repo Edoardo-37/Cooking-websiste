cfd23dbc2ddc9f5fc527527136b6bd2d84f14069
Edo37 <edoardo0307@gmail.com>
2025-04-03 12:51:59 +0200

    added recipe-card component with dynamic contents with props

---
+++ src/Components/Recipe-Card/RecipeCard.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import TimerIcon from '@mui/icons-material/Timer';
+import { typographyStyles } from '../common/typography/typographyStyles';
+import Link from '@mui/material/Link';
+
+
+export const RecipeCard = ({ recipe }: any) => {
+    const { label, description, dishType, prepTime, portions, img, alt, route, difficulty } = recipe;
+    return (
+        <Card sx={{
+            borderRadius: '14px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            cursor: 'pointer'
+        }}>
+            <CardMedia
+                component="img"
+                height="300px"
+                object-fit= 'contain'
+                image={img}
+                alt={alt}
+            />
+            <CardContent>
+                <Link href={route} sx={typographyStyles.recipesLink}>{label}</Link>
+            </CardContent>
+            <CardContent>
+                <Typography sx={typographyStyles.recipesDescription}>
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardContent>
+                <Typography variant="body1" sx={{
+                    backgroundColor: '#3D5926',
+                    color: '#ffffff',
+                    padding: '0.25rem 0.5rem',
+                    borderRadius: '0.25rem',
+                    width: 'fit-content'
+                }}>
+                    {dishType}
+                </Typography>
+            </CardContent>
+            <CardContent sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                gap: '0.5rem',
+            }}>
+            <div style={{display: 'flex', gap: '0.5rem', alignItems: 'center'}}>
+                <TimerIcon  />
+                <Typography sx={typographyStyles.recipesDescription}>{prepTime}</Typography>
+            </div>
+            <Typography sx={[typographyStyles.recipesDescription, {marginLeft: 'auto'}]}>Porzioni: <b style={{color: '#AE150D'}}>{portions}</b></Typography>
+            <Typography sx={typographyStyles.recipesDescription}>Difficoltà: <b style={{color: '#AE150D'}}>{difficulty}</b></Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
---
+++ src/Components/Recipe-Card/recipeCardContent.tsx
@@ -0,0 +1,71 @@
+export const recipeCardContent = {
+    zuppaDiPiselli:{
+        label: 'Zuppa di Piselli',
+        description: 'Con la loro dolcezza i piselli si adattano a moltissime preparazioni. Potete servire il piatto con crostini abbrustoliti in forno e conditi con olio extravergine di oliva. Piatto da gustare in una fredda serata invernale.',
+        dishType: 'Zuppa - 1° Piatto - Vegetariano',
+        prepTime: `40 minuti`,
+        difficulty: 'Facile',
+        portions: '4',
+        img: 'https://www.vibrantplate.com/wp-content/uploads/2018/06/Pea-soup-06.jpg',
+        alt: "Zuppa di piselli",
+        route: 'ricette/zuppa-di-piselli'
+    },
+    pastaAlPomodoro:{
+        label: 'Pasta al pomodoro',
+        description: 'Una ricetta semplice, ma tutt\'altro che banale. Soggetta ad infinite varianti per trovare l\'equilibrio perfetto di sapori, partendo già dalla scelta del tipo di pomodoro, da utilizzare per realizzare il sugo.',
+        dishType: 'Pasta - 1° Piatto - Vegetariano',
+        prepTime: '10 minuti',
+        difficulty: 'Facile',
+        portions: '2',
+        img: 'https://images.unsplash.com/photo-1630409352591-abc3cb4635de?q=80&w=1892&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        alt: "Pasta al pomodoro",
+        route: 'ricette/pasta-al-pomodoro'
+    },
+    risoZafferano:{
+        label: 'Risotto allo Zafferano',
+        description: 'Questo classico primo piatto è tipico della cucina lombarda dove viene spesso abbinato all’ossobuco, ma è amato in tutte le regioni d’Italia; si prepara con facilità e pochissimi ingredienti.',
+        dishType: 'Riso - 1° Piatto - Vegetariano',
+        prepTime: '35 minuti',
+        difficulty: 'Facile',
+        portions: '4',
+        img: 'https://www.donnamoderna.com/content/uploads/2023/05/3b5be886c5a53802-830x625.jpg',
+        alt: "Risotto allo Zafferano",
+        route: 'ricette/risotto-allo-zafferano'
+    },
+    bocconciniPolloAlLimone:{
+        label: 'Bocconcini di pollo al limone',
+        description: 'I bocconcini di petto di pollo al limone sono un secondo piatto facile che si prepara in poco tempo. Un’idea per il pranzo o la cena di tutti i giorni. Il succo di limone con la farina creerà una salsa gustosa, perfetta per fare scarpetta.',
+        dishType: 'Pollo - 2° Piatto - Tutte le stagioni',
+        prepTime: '15 minuti',
+        difficulty: 'Facile',
+        portions: '5',
+        img: 'https://www.cucchiaio.it/content/cucchiaio/it/ricette/2022/01/bocconcini-di-pollo-al-limone/jcr:content/page_images/ogImage.img10.jpg/1642750314192.jpg',
+        alt: "Bocconcini di petto di pollo al limone",
+        route: 'ricette/bocconcini-di-petto-di-pollo-al-limone'
+    },
+    involtiniDiZucchine:{
+        label: 'Involtini di zucchine al Forno',
+        description: 'Gli involtini di zucchine sono un antipasto  facile, goloso e pronto in meno di un’ora. Perfetti per un pranzo in famiglia o una cena tra amici da servire come primo piatto o come piatto unico. Un modo divertente per cucinare le zucchine con gusto.',
+        dishType: 'Zucchine - Antipasto',
+        prepTime: '45 minuti',
+        difficulty: 'Medio',
+        portions: '4',
+        img: 'https://www.tuorlorosso.it/wp-content/uploads/2023/04/Rotolini-di-zucchine-al-forno-2.jpg',
+        alt: "Involtini di zucchine al Forno",
+        route: 'ricette/involtini-di-zucchine-al-forno'
+    },
+    pastaFunghiPanna:{
+        label: 'Pasta funghi e panna',
+        description: 'Le pennette funghi e panna sono un primo piatto vegetariano molto semplice da preparare. Per la preparazione, possiamo utilizzare la panna vaccina, vegetale o di soia. Il piatto si prepara in poco tempo ed è considerabile un “salva cena”',
+        dishType: 'Pasta - 1° Piatto',
+        prepTime: '25 minuti',
+        difficulty: 'Facile',
+        portions: '2',
+        img: 'https://www.vasavasakitchen.com/wp-content/uploads/2022/11/pasta-funghi-speck-panna.jpg',
+        alt: "Pasta funghi e panna",
+        route: 'ricette/pennette-funghi-e-panna'
+    },
+};
+
+
+
