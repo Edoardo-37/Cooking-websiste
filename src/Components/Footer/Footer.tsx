c6ae52e522622a89f2e9bd6369a574c03d792905
Edo37 <edoardo0307@gmail.com>
2025-04-23 16:35:33 +0200

    removed css file for component and added inline style

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,10 +1,9 @@
 import { Typography } from '@mui/material';
 import { typographyStyles } from '../common/typography/typographyStyles';
-import './footer.css'
 
 export const Footer = () => {
     return (
-        <footer>
+        <footer style={{background: '#4974a5', width: '100%'}}>
             <Typography sx={typographyStyles.pHero}> Copyright &copy;2025 -  Edoardo Massimetti </Typography>
         </footer>
     )
