87864cd9f44ad543ad07b7016d8e5da459db6801
Edo37 <edoardo0307@gmail.com>
2025-06-30 13:21:49 +0200

    added cursor pointer prop to navLinks

--- src/Components/Navbar/navStyles.tsx
+++ src/Components/Navbar/navStyles.tsx
@@ -24,6 +24,7 @@ export const navStyles = {
         color: '#000000',
         fontWeight: 'bold',
         fontSize: '1rem',
+        cursor: 'pointer',
         display:{
             xs: 'none',
             sm: 'none',
